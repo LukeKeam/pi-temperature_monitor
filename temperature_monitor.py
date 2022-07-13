347006b393a1b55e62950ebd20aa433d2e117eea
Lukes <luke_keam@hotmail.com>
2022-07-14 09:49:50 +1000

    .113

--- temperature_monitor.py
+++ temperature_monitor.py
@@ -6,7 +6,7 @@ from log_write_to_text_file import log_write_to_text_file
 
 def measure_temperature():
         while True:
-                temperature = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
+                temperature = os.popen("vcgencmd measure_temp").readline()
                 temperature = (temperature.replace("temp=",""))
                 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 print(now, temperature)
