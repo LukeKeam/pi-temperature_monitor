7a11afd29429285fc112e678301f5b68ad6c0805
Lukes <luke_keam@hotmail.com>
2022-07-14 10:04:58 +1000

    .12

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ https://techgeek.biz/techtips/pi-temperature_monitor
 # easy install & run
 sudo apt-get install git -y && git clone https://github.com/LukeKeam/pi-temperature_monitor && cd pi-temperature_monitor && python3 temperature_monitor.py
 
+Tested on Raspberry Pi 4
+Line 9 of temperature_monitor.py may change location of temperature in future 
+temperature = os.popen("vcgencmd measure_temp").readline()
--- log_write_to_text_file.py
+++ log_write_to_text_file.py
@@ -3,17 +3,16 @@ import datetime
 
 
 def log_write_to_text_file(log_message):
+    # make txt file for day
     datetime_log = datetime.datetime.now()
     datetime_log_date = datetime_log.strftime("%Y-%m-%d")
+    # check for Log folder
     path_test = os.path.exists('./Log')
     if path_test == False:
         os.mkdir('./Log')
+    # write to txt file
     log_write = open("./Log/Log {0}.txt".format(datetime_log_date), "a")
     datetime_log_message = datetime_log.strftime("%Y-%m-%d %H:%M:%S")
-    log_write.write(datetime_log_message)
-    log_write.write(': ')
-    log_write.write(log_message)
-    log_write.write("\n")
-    # doesn't write until closed end
+    log_write.write(datetime_log_message, ': ', log_message, "\n")
     log_write.close()
 
